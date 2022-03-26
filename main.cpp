a6e436395cf66bac14c805ed9dab78c14414edc7
Nicholas Mirchandani <nmirchandani@chapman.edu>
2022-03-26 16:39:52 -0700

    WS2_32 testing

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
+// NOTE: Windows needs to be compiled with -lWS2_32 for the WS2_32 library
+
 #ifdef _WIN32
 #include <winsock2.h>
 #include <Ws2tcpip.h>
 
-// Wanted to use SOCKET but that conflicted with mingW
+// Wanted to use SOCKET but that conflicted with mingW.  Unsure if SOCKET is defined in posix
 typedef unsigned int SOCKET_TYPE;
 #else
 // Currently assuming non-windows is POSIX, which may not be a safe assumption
@@ -18,7 +20,25 @@ typedef int SOCKET_TYPE;
 #include <iostream>
 
 int main() {
-    SOCKET_TYPE test;
-    test = -3;
-    std::cout << "Hello World! " << test << std::endl;
+    #ifdef _WIN32
+    WSADATA wsa;
+	printf("\nInitialising Winsock...");
+	if (WSAStartup(MAKEWORD(2,2),&wsa) != 0)
+	{
+		printf("Failed. Error Code : %d",WSAGetLastError());
+		return 1;
+	}
+	printf("Initialised.\n");
+	#endif
+
+	SOCKET_TYPE s;
+	
+	if((s = socket(AF_INET , SOCK_STREAM , 0 )) == INVALID_SOCKET)
+	{
+		printf("Could not create socket : %d" , WSAGetLastError());
+	}
+
+	printf("Socket created.\n");
+
+	return 0;
 }
