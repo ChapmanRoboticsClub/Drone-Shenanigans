da606fb86e06a615b011e6c3165f70baac211f45
Nicholas Mirchandani <nmirchandani@chapman.edu>
2022-03-26 16:29:43 -0700

    Verifies typedef of SOCKET_TYPE

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
+#ifdef _WIN32
+#include <winsock2.h>
+#include <Ws2tcpip.h>
+
+// Wanted to use SOCKET but that conflicted with mingW
+typedef unsigned int SOCKET_TYPE;
+#else
+// Currently assuming non-windows is POSIX, which may not be a safe assumption
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+
+typedef int SOCKET_TYPE;
+#endif
+
+
 #include <iostream>
 
 int main() {
-    #ifdef _WIN32
-    std::cout << "Hello World!" << std::endl;
-    #endif
+    SOCKET_TYPE test;
+    test = -3;
+    std::cout << "Hello World! " << test << std::endl;
 }
